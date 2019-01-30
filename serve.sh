4d3bde2447988d154e7c7bd8736ac31ee8443fe9
Eric Engstrom <eengstrom@users.noreply.github.com>
2019-01-29 23:59:31 -0600

    a little more structure to local test script

--- serve.sh
+++ serve.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-set -e
-set -x
+PROGNAME=$(basename "$0")
+warn()  { echo "$PROGNAME: ${@}" 1>&2; }
+die()   { warn "${@}"; exit 1; }
+# usage() { echo "usage: $PROGNAME <MEETING-TYPE>" 1>&2; }
 
+set -e  # exit on error
+#set -x  # print each line as executed
+
+# setup exit trap to kill all subjobs
+trap 'kill $(jobs -p) 2>/dev/null' EXIT
+trap 'exit' SIGHUP SIGINT SIGQUIT SIGTERM
+
+# Update jekyll/ruby modules and start server
 bundle update
-bundle exec jekyll serve
+bundle exec jekyll serve &
+
+sleep 2
+
+URL="http://localhost:4000"
+open ${URL} \
+  || start ${URL} \
+  || echo "open browser and point at '$URL'"
+
+# start a trivial subjob that is long lived
+#sleep 99999 &
+
+# Wait until the user 'terminates' the meeeting
+echo "Press Ctrl-C to terminate..."
+wait
